5a540dcf101fed28cc2ac1eace7f6fb97be3ac81
radkin <noeldamonmiller@gmail.com>
2020-03-14 08:57:10 -0700

    inintial_material_ui hello world with mobile geometry and basic button
    very basic, crawling with material here.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,44 @@ import { configure } from 'mobx'
 
 import GetmeList from './GetmeList'
 import uiState from './uiState'
+import Button from '@material-ui/core/Button';
+import * as serviceWorker from './serviceWorker';
+import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
 
 configure(true)
 
+// material-ui hello world button
+function App() {
+  return (
+    <ScopedCssBaseline>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </ScopedCssBaseline>
+  );
+}
+
+// 'response meta tag' for mobile compatibility
+const headElement =
+  <meta
+    name="viewport"
+    content="minimum-scale=1, initial-scale=1, width=device-width"
+  />;
+
+/* DOM render tree */
+
+// 'responsive meta tag'
+ReactDOM.render(headElement, document.getElementById('root'));
+
+// getme apollo client observer
 ReactDOM.render(
   <GetmeList uiState={uiState} />,
   document.getElementById('root')
 )
+
+// serviceWorker for offline and load faster
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
 
 /*
 import React from 'react';
