378e60998cda7d9bbb56e8afe5bdd98a468c16fc
radkin <noeldamonmiller@gmail.com>
2020-03-24 12:30:36 -0700

    initial_material_ui step closer to grid and drawer
    getting grid and the material-ui drawer to play nicely together is
    proving to be a challenge. In this commit we have the header and drawer
    integrated, but the content for the drawer is the only part moving out
    of the way. The grid does not appear to be obey the drawer

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,48 @@
 import React, { Component } from 'react';
+// import {
+//   Route,
+//   NavLink,
+//   HashRouter
+// } from 'react-router-dom';
 import {
-  Route,
-  NavLink,
   HashRouter
 } from 'react-router-dom';
-import Home from './Home';
-import Stuff from './Stuff';
-import Contact from './Contact';
+// import Home from './Home';
+// import Stuff from './Stuff';
+// import Contact from './Contact';
+import Demo from './demo';
+
+// class Main extends Component {
+//   render() {
+//     return (
+//       <HashRouter>
+//         <div>
+//           <h1>Simple SPA</h1>
+//           <ul className="header">
+//             <li><NavLink exact to="/">Home</NavLink></li>
+//             <li><NavLink to="/stuff">Stuff</NavLink></li>
+//             <li><NavLink to="/contact">Contact</NavLink></li>
+//           </ul>
+//           <div className="content">
+//             <Route exact path="/" component={Home}/>
+//             <Route path="/stuff" component={Stuff}/>
+//             <Route path="/contact" component={Contact}/>
+//           </div>
+//         </div>
+//       </HashRouter>
+//     );
+//   }
+// }
 
 class Main extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/stuff">Stuff</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/stuff" component={Stuff}/>
-            <Route path="/contact" component={Contact}/>
-          </div>
+          <Demo />
         </div>
       </HashRouter>
-    );
+    )
   }
 }
-
 export default Main;
