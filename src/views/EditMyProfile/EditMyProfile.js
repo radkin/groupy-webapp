457a3944a571f7ddd630d828676b7ac2b4f53372
radkin <noeldamonmiller@gmail.com>
2020-04-10 14:18:36 -0700

    view_my_profile tha basics
    we have the fields and they are roughly where they need to be. There is
    no pre-populated text, no way to edit what's there right now, etc.

--- src/views/EditMyProfile/EditMyProfile.js
+++ src/views/EditMyProfile/EditMyProfile.js
@@ -1,8 +1,15 @@
+/* eslint no-undef: 0 */
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
-
+import $ from 'jquery';
+import { Grid, TextField } from '@material-ui/core';
 // import { Notifications, Password } from './components';
+// Apollo
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import * as queries from '../../graphql/queries';
+
+const meQuery = gql(queries.users.getMe.graphql);
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,25 +20,58 @@ const useStyles = makeStyles(theme => ({
 const EditMyProfile = () => {
   const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Grid
-        container
-        spacing={4}
-      >
-        <Grid>
-          item
-          md={7}
-          xs={12}
-        </Grid>
-        <Grid>
-          item
-          md={5}
-          xs={12}
-        </Grid>
-      </Grid>
-    </div>
-  );
+  // DATA BINDING
+  const { loading, error, data } = useQuery(meQuery);
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  if (loading) {
+    return (
+      <div className="App">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+  if (data) {
+    console.log('user is', data.me);
+    // if (data.user.length > 0) {
+    if (!$.isEmptyObject(data.me)) {
+      return (
+        <div className={classes.root}>
+          <Grid
+            container
+            spacing={4}
+          >
+            <Grid>
+              <form
+                autoComplete="off"
+                className={classes.root}
+                noValidate
+              >
+                <TextField id="standard-basic" label="First Name" />
+                <TextField id="standard-basic" label="Last Name" />
+              </form>
+            </Grid>
+            <Grid>
+              <form
+                autoComplete="off"
+                className={classes.root}
+                noValidate
+              >
+                <TextField
+                  helperText="to help you find groups"
+                  id="standard-basic"
+                  label="Zip Code" 
+                />
+                <TextField id="standard-basic" label="Initials" />
+              </form>
+            </Grid>
+          </Grid>
+        </div>
+      );
+    } // data-binding logic
+  }
 };
 
 export default EditMyProfile;
