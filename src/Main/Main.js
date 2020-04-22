971aa1bd6989a7cca7c6a65639c1fc5af65fcad1
radkin <noeldamonmiller@gmail.com>
2020-04-22 16:16:40 -0700

    fix_routing1 now we can see the selected pages in the right side
    geometry is working and the sidebar nav is playing nicely with our
    routes. The next trick is to set up the default route so that it does
    not show a 404 on the initial page load

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,8 @@
 /* eslint no-undef: 0 */
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { Link as RouterLink } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-static';
 // MAterial UI generic
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
@@ -14,6 +15,7 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 // custom imports
 import { Profile, SidebarNav } from './components';
 import { EditMyProfile as EditMyProfileView } from '../views';
+import { Setup as SetupView } from '../views';
 
 const drawerWidth = 240;
 
@@ -90,73 +92,75 @@ const Main = props => {
     setOpen(false);
   };
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-        position="fixed"
-      >
-        <Toolbar>
-          <IconButton
-            aria-label="open drawer"
-            className={clsx(classes.menuButton, open && classes.hide)}
-            color="inherit"
-            edge="start"
-            onClick={handleDrawerOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            noWrap
-            variant="h6"
-          >
-            <img src={require('../assets/groupy_yellow.png')} />
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        anchor="left"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        className={classes.drawer}
-        open={open}
-        variant="persistent"
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
+    <BrowserRouter>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+          position="fixed"
+        >
+          <Toolbar>
+            <IconButton
+              aria-label="open drawer"
+              className={clsx(classes.menuButton, open && classes.hide)}
+              color="inherit"
+              edge="start"
+              onClick={handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              noWrap
+              variant="h6"
+            >
+              <img src={require('../assets/groupy_yellow.png')} />
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          anchor="left"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          className={classes.drawer}
+          open={open}
+          variant="persistent"
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+            <Divider />
+            <Profile />
+          </div>
           <Divider />
-          <Profile />
-        </div>
-        <Divider />
-        <SidebarNav />
-        <Divider />
-      </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
-        <div className={classes.drawerHeader} />
-        <EditMyProfileView />
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-        {children}
-      </main>
-    </div>
+          <SidebarNav />
+          <Divider />
+        </Drawer>
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader} />
+          <Switch>
+            <Route
+              component={EditMyProfileView}
+              path="/editmyprofile"
+            />
+            <Route
+              component={SetupView}
+              path="/setup"
+            />
+            <Route component={() => (<div>404 Not found </div>)} />
+            <Route render={() => <Routes />} />
+          </Switch>
+          {children}
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
