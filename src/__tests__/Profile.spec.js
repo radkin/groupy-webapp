85dc12197a4cd8576d66b02ac19d0d9900400b84
radkin <noeldamonmiller@gmail.com>
2020-05-01 13:39:53 -0700

    testing1 testing me object and value for first
    very basic snapshot testing

--- src/__tests__/Profile.spec.js
+++ src/__tests__/Profile.spec.js
@@ -2,4 +2,16 @@ import me from './data';
 
 test('proper profile data is correct', () => {
   expect(me).toMatchSnapshot();
+  expect(me.first).toEqual('jon');
 });
+
+test('me object should have properties id, first, last, zipCode, color,\
+  initials, profileImage', () => {
+    expect(me).toHaveProperty('id');
+    expect(me).toHaveProperty('first');
+    expect(me).toHaveProperty('last');
+    expect(me).toHaveProperty('zipCode');
+    expect(me).toHaveProperty('color');
+    expect(me).toHaveProperty('initials');
+    expect(me).toHaveProperty('profileImage');
+  });
