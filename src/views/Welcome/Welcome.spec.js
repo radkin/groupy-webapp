919750cedce3fc635a2f1b9b3519b60c5e639912
radkin <noeldamonmiller@gmail.com>
2020-05-09 21:03:06 -0700

    improve_coverage2 tests for elements in our welcome views

--- src/views/Welcome/Welcome.spec.js
+++ src/views/Welcome/Welcome.spec.js
@@ -1,76 +1,60 @@
 import React, { useState } from 'react';
-import { TextField } from '@material-ui/core';
-import MockTheme from '../../testComponents/MockTheme';
+import { TextField, Typography, Button, Paper, Grid } from '@material-ui/core';
 
 // testing libraries
+import { render, fireEvent } from '@testing-library/react';
 import Enzyme from 'enzyme';
 import { mount } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-// import { shallow } from 'enzyme';
+// custom Mock
+import MockTheme from '../../testComponents/MockTheme';
 import { createShallow } from '@material-ui/core/test-utils';
 
 import Welcome from './Welcome';
 
-// it('should disable submit button on submit click', () => {
-//   const wrapper = mount(<Welcome />);
-//   const submitButton = wrapper.find(Button);
-//   submitButton.simulate('click');
-//
-//   expect(submitButton.prop('disabled')).toBeTruthy();
-// });
-
 describe('<Welcome /> with no props', () => {
-  // const container = createShallow(<Welcome />);
-  // it('should match the snapshot', () => {
-  //   expect(container.html()).toMatchSnapshot();
-  // });
-
-  it("should render <TextField />", () => {
-
+  it('should render <TextField>', () => {
     const wrapper = mount(
       <MockTheme>
          <Welcome />
       </MockTheme>,
      );
-
-     // const wrapper = mount(<Welcome />);
      expect(wrapper.find(TextField)).toHaveLength(1);
      // expect(wrapper.find(TextField).props().InputProps.disableUnderline).toBe(
      //   true
      // );
    });
-
-  // it('should have an email field', () => {
-  //   expect(container.find('Email').length).toEqual(1);
-  // });
-  //
-  // it('should have proper props for email field', () => {
-  //   expect(container.find('Email').props()).toEqual({
-  //     onBlur: expect.any(Function),
-  //     isValid: false,
-  //   });
-  // });
-  //
-  // it('should have a password field', () => {
-  //   expect(container.find('Password').length).toEqual(1);
-  // });
-  //
-  // it('should have proper props for password field', () => {
-  //   expect(container.find('Password').props()).toEqual({
-  //     onChange: expect.any(Function),
-  //     value: '',
-  //   });
-  // });
-
-  // it('should have a submit button', () => {
-  //   expect(container.find('Button').length).toEqual(1);
-  // });
-  //
-  // it('should have proper props for submit button', () => {
-  //   expect(container.find('Button').props()).toEqual({
-  //     disabled: true,
-  //     onClick: expect.any(Function),
-  //   });
-  // });
+  it('should render <Typography>', () => {
+    const wrapper = mount(
+      <MockTheme>
+        <Welcome />
+      </MockTheme>,
+    );
+    expect(wrapper.find(Typography)).toHaveLength(4);
+  });
+  it('should render <Button>', () => {
+    const wrapper = mount(
+      <MockTheme>
+        <Welcome />
+      </MockTheme>,
+    );
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+  it('should render <Paper>', () => {
+    const wrapper = mount(
+      <MockTheme>
+        <Welcome />
+      </MockTheme>,
+    );
+    expect(wrapper.find(Paper)).toHaveLength(1);
+  });
+  it('should render <Grid>', () => {
+    const wrapper = mount(
+      <MockTheme>
+        <Welcome />
+      </MockTheme>,
+    );
+    expect(wrapper.find(Grid)).toHaveLength(2);
+  });
 });
