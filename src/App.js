dc7099b68eb003fdc464017c64b07c28ead299b7
radkin <noeldamonmiller@gmail.com>
2020-05-14 12:58:18 -0700

    apollo_state tests working
    working state "starter" for adding new tests in this branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 /* eslint no-undef: 0 */
-import React, { Suspense, Component } from 'react';
-import { Root, Routes } from 'react-static';
+import React, { Component } from 'react';
+import { Routes } from 'react-static';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Main from './Main';
-// apollo
-import { ApolloProvider } from '@apollo/react-hooks';
-import client from './graphql/client';
-// material-ui
-import { ThemeProvider } from '@material-ui/styles';
-import theme from './theme';
 // Custom
 import { EditMyProfile as EditMyProfileView } from './views';
 import { Setup as SetupView } from './views';
@@ -20,52 +14,43 @@ import { JoinGroup as JoinGroupView } from './views';
 class App extends Component {
   render() {
     return (
-      <Root>
-        <ApolloProvider client={client}>
-          <ThemeProvider theme={theme}>
-            <Suspense fallback={<div>Loading... </div>}>
-              <BrowserRouter>
-                <Switch>
-                  <Route
-                    exact
-                    path="/"
-                  >
-                    <Main />
-                  </Route>
-                  <Route
-                    component={EditMyProfileView}
-                    path="/editmyprofile"
-                  />
-                  <Route
-                    component={SetupView}
-                    path="/setup"
-                  />
-                  <Route
-                    component={CreateGroupView}
-                    path="/creategroup"
-                  />
-                  <Route
-                    component={JoinGroupView}
-                    path="/joingroup"
-                  />
-                  <Route
-                    component={UpcomingEventsView}
-                    path="/upcomingevents"
-                  />
-                  <Route
-                    component={MessagesView}
-                    path="/messages"
-                  />
-                  <Route component={() => (<div>404 Not found </div>)} />
-                  <Route render={() => <Routes />} />
-                </Switch>
-              </BrowserRouter>
-            </Suspense>
-          </ThemeProvider>
-        </ApolloProvider>
-      </Root>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+          >
+            <Main />
+          </Route>
+          <Route
+            component={EditMyProfileView}
+            path="/editmyprofile"
+          />
+          <Route
+            component={SetupView}
+            path="/setup"
+          />
+          <Route
+            component={CreateGroupView}
+            path="/creategroup"
+          />
+          <Route
+            component={JoinGroupView}
+            path="/joingroup"
+          />
+          <Route
+            component={UpcomingEventsView}
+            path="/upcomingevents"
+          />
+          <Route
+            component={MessagesView}
+            path="/messages"
+          />
+          <Route component={() => (<div>404 Not found </div>)} />
+          <Route render={() => <Routes />} />
+        </Switch>
+      </BrowserRouter>
     )
-
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint no-undef: 0 */
+
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Root, Routes } from 'react-static';
@@ -8,39 +10,97 @@ import localStorage from 'localStorage';
 import App from './App';
 import { Welcome as WelcomeView } from './views';
 // apollo
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink
+} from 'apollo-boost';
+import { persistCache as PersistCache } from 'apollo-cache-persist';
 import { ApolloProvider } from '@apollo/react-hooks';
-import client from './graphql/client';
 // material-ui
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
 
-function Greeting() {
-  // Do we have existing localStorage?
-  const weHaveLocalStorage = JSON.parse(localStorage.getItem('groupy'));
-  if (weHaveLocalStorage) {
-    return <App />;
+const init = async () => {
+  const server = process.env.REACT_APP_GROUPY_GRAPHQL_SERVER;
+  const port = process.env.REACT_APP_PORTNUM;
+  // set up protocol
+  let transferProtocol = 'https';
+  if (process.env.REACT_APP_PORTNUM) {
+    transferProtocol = 'http';
   }
-  return (
-    <Root>
-      <ApolloProvider client={client}>
-        <ThemeProvider theme={theme}>
-          <Suspense fallback={<div>Loading... </div>}>
-            <BrowserRouter>
-              <Switch>
-                <Route
-                  component={WelcomeView}
-                  path="/"
-                />
-                <Route component={() => (<div>404 Not found </div>)} />
-                <Route render={() => <Routes />} />
-              </Switch>
-            </BrowserRouter>
-          </Suspense>
-        </ThemeProvider>
-      </ApolloProvider>
-    </Root>
-  );
-}
+  // persist cache in Apollo
+  const cache = new InMemoryCache();
+  const persistCache = new PersistCache({
+    cache,
+    storage: window.localStorage,
+  });
+  const ls = await JSON.parse(localStorage.getItem('groupy'));
+  const token = ls.token;
 
-ReactDOM.render(<Greeting  />, document.getElementById('root'));
-serviceWorker.unregister();
+  const client = new ApolloClient({
+    cache: cache,
+    persistCache: persistCache,
+    link: new ApolloLink((operation, forward) => {
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : '',
+        }
+      });
+      return forward(operation);
+    }).concat(
+      new HttpLink({
+        uri: `${transferProtocol}://${server}:${port}/graphql`,
+        credentials: 'same-origin',
+        resolvers: {},
+      })
+    ),
+  });
+
+  const Greeting = () => {
+    // Do we have existing localStorage?
+    const weHaveLocalStorage = JSON.parse(localStorage.getItem('groupy'));
+    if (weHaveLocalStorage) {
+      return (
+        <Root>
+          <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+              <Suspense fallback={<div>Loading... </div>}>
+                <App />
+              </Suspense>
+            </ThemeProvider>
+          </ApolloProvider>
+        </Root>
+      );
+    }
+    return (
+      <Root>
+        <ApolloProvider client={client}>
+          <ThemeProvider theme={theme}>
+            <Suspense fallback={<div>Loading... </div>}>
+              <BrowserRouter>
+                <Switch>
+                  <Route
+                    component={WelcomeView}
+                    path="/"
+                  />
+                  <Route component={() => (<div>404 Not found </div>)} />
+                  <Route render={() => <Routes />} />
+                </Switch>
+              </BrowserRouter>
+            </Suspense>
+          </ThemeProvider>
+        </ApolloProvider>
+      </Root>
+    );
+  }
+
+  ReactDOM.render(<Greeting  />, document.getElementById('root'));
+  serviceWorker.unregister();
+
+};
+
+
+
+init();
